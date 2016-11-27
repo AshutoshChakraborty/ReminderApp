4439defbe972d19f47636d4370bccaf51fa61c55
AshutoshChakraborty <ashutoshmca13@gmail.com>
2016-11-27 21:24:10 +0530

    declare an image view

--- app/src/main/java/com/example/ashutosh/reminderapp/MainActivity.java
+++ app/src/main/java/com/example/ashutosh/reminderapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.ashutosh.reminderapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
